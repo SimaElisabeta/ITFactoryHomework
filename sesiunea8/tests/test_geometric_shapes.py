4ff03253c4d17452cf7e5a3fa91267af5e00c70e
SimaElisabeta <sima.elisabeta@gmail.com>
2023-01-18 17:10:17 +0200

    reduced some code repetition

--- sesiunea8/tests/test_geometric_shapes.py
+++ sesiunea8/tests/test_geometric_shapes.py
@@ -5,62 +5,53 @@ from sesiunea8.app.geometric_shapes import Square, Rectangle, Circle
 
 class TestSquare(unittest.TestCase):
     @parameterized.expand([
-        (12.5, 156.25),
-        (10, 100)
+        (Square(12.5), 156.25),
+        (Square(10), 100)
     ])
-    def test_area(self, value, expected):
-        square = Square(value)
+    def test_area(self, square, expected):
         actual = round(square.area(), 2)  # use precision of 2 decimal points
         self.assertEqual(actual, expected)
 
     @parameterized.expand([
-        (12.5, 50),
-        (10, 40)
+        (Square(12.5), 50),
+        (Square(10), 40)
     ])
-    def test_perimeter(self, value, expected):
-        square = Square(value)
+    def test_perimeter(self, square, expected):
         actual = round(square.perimeter(), 2)
         self.assertEqual(actual, expected)
 
 
 class TestRectangle(unittest.TestCase):
     @parameterized.expand([
-        (30, 15, 450),
-        (45.50, 10.50, 477.75)
+        (Rectangle(30, 15), 450),
+        (Rectangle(45.50, 10.50), 477.75)
     ])
-    def test_area(self, length, width, expected):
-        rectangle = Rectangle(length, width)
+    def test_area(self, rectangle, expected):
         actual = round(rectangle.area(), 2)
         self.assertEqual(actual, expected)
 
     @parameterized.expand([
-        (30, 15, 90),
-        (45.50, 10.10, 111.20)
+        (Rectangle(30, 15), 90),
+        (Rectangle(45.50, 10.10), 111.20)
     ])
-    def test_perimeter(self, length, width, expected):
-        rectangle = Rectangle(length, width)
+    def test_perimeter(self, rectangle, expected):
         actual = round(rectangle.perimeter(), 2)
         self.assertEqual(actual, expected)
 
 
 class TestCircle(unittest.TestCase):
-    def setUp(self):
-        self.circle = Circle(20)
-
     @parameterized.expand([
-        (20, 1256.637),
-        (3.5, 38.485)
+        (Circle(20), 1256.637),
+        (Circle(3.5), 38.485)
     ])
-    def test_area(self, radius, expected):
-        circle = Circle(radius)
+    def test_area(self, circle, expected):
         actual = round(circle.area(), 3)  # use precision of 3 decimal points
         self.assertEqual(actual, expected)
 
     @parameterized.expand([
-        (5, 31.416),
-        (3.5, 21.991)
+        (Circle(5), 31.416),
+        (Circle(3.5), 21.991)
     ])
-    def test_perimeter(self, radius, expected):
-        circle = Circle(radius)
+    def test_perimeter(self, circle, expected):
         actual = round(circle.perimeter(), 3)
         self.assertEqual(actual, expected)
--- sesiunea8/tests/test_geometric_shapes_pytest.py
+++ sesiunea8/tests/test_geometric_shapes_pytest.py
@@ -3,60 +3,54 @@ from sesiunea8.app.geometric_shapes import Square, Rectangle, Circle
 
 
 class TestSquare:
-    @pytest.mark.parametrize('value, expected', [
-        (12.5, 156.25),
-        (10, 100)
+    @pytest.mark.parametrize('square, expected', [
+        (Square(12.5), 156.25),
+        (Square(10), 100)
     ])
-    def test_area(self, value, expected):
-        square = Square(value)
+    def test_area(self, square, expected):
         actual = round(square.area(), 2)
         assert actual == expected
 
-    @pytest.mark.parametrize("value, expected", [
-        (12.5, 50),
-        (10, 40)
+    @pytest.mark.parametrize("square, expected", [
+        (Square(12.5), 50),
+        (Square(10), 40)
     ])
-    def test_perimeter(self, value, expected):
-        square = Square(value)
+    def test_perimeter(self, square, expected):
         actual = round(square.perimeter(), 2)
         assert actual == expected
 
 
 class TestRectangle:
-    @pytest.mark.parametrize("length, width, expected", [
-        (30, 15, 450),
-        (45.50, 10.50, 477.75)
+    @pytest.mark.parametrize("rectangle, expected", [
+        (Rectangle(30, 15), 450),
+        (Rectangle(45.50, 10.50), 477.75)
     ])
-    def test_area(self, length, width, expected):
-        rectangle = Rectangle(length, width)
+    def test_area(self, rectangle, expected):
         actual = round(rectangle.area(), 2)
         assert actual == expected
 
-    @pytest.mark.parametrize("length, width, expected", [
-        (30, 15, 90),
-        (45.50, 10.10, 111.20)
+    @pytest.mark.parametrize("rectangle, expected", [
+        (Rectangle(30, 15), 90),
+        (Rectangle(45.50, 10.10), 111.20)
     ])
-    def test_perimeter(self, length, width, expected):
-        rectangle = Rectangle(length, width)
+    def test_perimeter(self, rectangle, expected):
         actual = round(rectangle.perimeter(), 2)
         assert actual == expected
 
 
 class TestCircle:
-    @pytest.mark.parametrize("radius, expected", [
-        (20, 1256.637),
-        (3.5, 38.485)
+    @pytest.mark.parametrize("circle, expected", [
+        (Circle(20), 1256.637),
+        (Circle(3.5), 38.485)
     ])
-    def test_area(self, radius, expected):
-        circle = Circle(radius)
+    def test_area(self, circle, expected):
         actual = round(circle.area(), 3)
         assert actual == expected
 
-    @pytest.mark.parametrize("radius, expected", [
-        (5, 31.416),
-        (3.5, 21.991)
+    @pytest.mark.parametrize("circle, expected", [
+        (Circle(5), 31.416),
+        (Circle(3.5), 21.991)
     ])
-    def test_perimeter(self, radius, expected):
-        circle = Circle(radius)
+    def test_perimeter(self, circle, expected):
         actual = round(circle.perimeter(), 3)
         assert actual == expected
